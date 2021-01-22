ce5e90595f6abfe42d530daedf5b93962c5fd99c
Mateus Henrique <mateus.magalhaes@upflow.me>
2021-01-21 23:49:12 -0300

    Adição da estrutura de pastas

---
+++ src/routes.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const AppStack = createStackNavigator();
+
+import Login from "./pages/Login/Login";
+// import { BuscarVoos } from "./pages/BuscarVoos";
+// import { MeusVoos } from "./pages/MeusVoos";
+// import { Pagamento } from "./pages/Pagamento";
+export default function Routes() {
+	return (
+		<NavigationContainer>
+			<AppStack.Navigator>
+				<AppStack.Screen name="Login" component={Login} />
+				{/* <AppStack.Screen name="BuscarVoos" component={BuscarVoos} />
+				<AppStack.Screen name="MeusVoos" component={MeusVoos} />
+				<AppStack.Screen name="Pagamento" component={Pagamento} /> */}
+			</AppStack.Navigator>
+		</NavigationContainer>
+	);
+}
