dc9d00d35305ba61b9bda1576157907c0df11fb9
Mateus Henrique <mateus.magalhaes@upflow.me>
2021-02-10 22:31:01 -0300

    Ajuste no moneyBR

--- src/Utils/index.tsx
+++ src/Utils/index.tsx
@@ -1,9 +1,10 @@
 function moneyBR(numero : number) : string{
 	const numDoisDecimais = (Math.round(numero*100)/100).toFixed(2)
 	// coloca os pontos a cada 3 digitos e troca o último ponto por vírgula
-	const regex = /(\d{3})(?=.*\d{3}\.)/g
-	const num = numDoisDecimais.replace(regex,'$1.').replace(/\.(?!.*\.)/,',') 
-		
+	const regex = /(\d)(?=(\d{3})+\.)/g
+	const num = numDoisDecimais
+		.replace(regex,'$1.')
+		.replace(/\.(?!.*\.)/,',')
 	return `R$ ${num}`
 }
 export {moneyBR}
